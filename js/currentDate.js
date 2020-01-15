2820410d5f652b63ed50caabd16245713401fd3e
Nwokolo24 <nwokolobueze@gmail.com>
2020-01-15 09:12:27 -0700

    I worked on the color choice of my page

--- js/currentDate.js
+++ js/currentDate.js
@@ -1,3 +1,1 @@
-// const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
-// document.getElementById("currentdate").textContent = new Date().toTimeString("en-US", options);
 document.getElementById("currentdate").innerHTML = document.lastModified;
