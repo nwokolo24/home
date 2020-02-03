cdfbd8c25a4fc0a7e71c2351bf129bb5913b357e
Nwokolo24 <nwokolobueze@gmail.com>
2020-02-03 08:24:38 -0700

    incerted some javaScript

--- js/currentDate.js
+++ js/currentDate.js
@@ -1,1 +1,22 @@
-document.getElementById("currentdate").innerHTML = document.lastModified;
+//Js to get the actual Year
+var today = new Date();
+var month = today.getMonth();
+var day = today.getDay();
+var year = today.getFullYear();
+document.getElementById("currentdate").innerHTML = day + "/" + month + "/"  + year;
+
+//Js to get the last modified date
+document.getElementById("lastmodify").innerHTML = document.lastModified;
+
+/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+function burgerMenu() {
+    var naviBar = document.getElementsById("navbar");
+    var footerBar = document.getElementsById("footer");
+    if (naviBar.className === "hide") {
+      naviBar.className = "shown";
+      footerBar.className = "large-footer";
+    } else {
+        naviBar.className = "hide";
+        footerBar.className = "minorfooter";
+    }
+  } 
