9c636a32679d6c6dd1303a643c24481f3e96f1d8
Nwokolo24 <nwokolobueze@gmail.com>
2020-02-05 01:11:13 -0700

    wrote css for small and large views

---
+++ weather/js/preston.js
@@ -0,0 +1,11 @@
+//Js to get the Current Date
+const options = {weekday: "long", day: "numeric", month:"short", year: "numeric"};
+document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+
+//Js to get the last modified date
+document.getElementById("lastmodify").innerHTML = document.lastModified;
+
+/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+function burgerMenu() {
+   document.getElementById("myLinks").classList.toggle("hide");
+  }
