4a9884d55a4429d650b7613aa39ddd35aee8ed2f
Nwokolo24 <nwokolobueze@gmail.com>
2020-02-14 09:46:56 -0700

    webfont loader

--- js/currentDate.js
+++ js/currentDate.js
@@ -4,16 +4,3 @@ document.getElementById("currentdate").textContent = new Date().toLocaleDateStri
 
 //Js to get the last modified date
 document.getElementById("lastmodify").innerHTML = document.lastModified;
-
-/* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
-function burgerMenu() {
-    var naviBar = document.getElementsById("navbar");
-    var footerBar = document.getElementsById("footer");
-    if (naviBar.className === "hide") {
-      naviBar.className = "shown";
-      footerBar.className = "large-footer";
-    } else {
-        naviBar.className = "hide";
-        footerBar.className = "minorfooter";
-    }
-  } 
