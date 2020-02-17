e379fa52472653c9bdac06598826c970d7a32265
Nwokolo24 <nwokolobueze@gmail.com>
2020-02-17 16:35:25 -0700

    lazy image loading

--- weather/js/images.js
+++ weather/js/images.js
@@ -1,32 +1,30 @@
-// get all Image with data-src attribute
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-
-// optional parameters being set for the intesectional Observer
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
-};
-
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {image.removeAttribute("data-src");};
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
+
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
 
 // first check to see if intersection Observer is supported
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+// if('IntersectionObserver' in window) {
+//     const observer = new IntersectionObserver((items, observer) => {
+//       items.forEach((item) => {
+//         if(item.isIntersecting) {
+//           loadImages(item.target);
+//           observer.unobserve(item.target);
+//         }
+//       });
+//     });
+//     imagesToLoad.forEach((img) => {
+//       observer.observe(img);
+//     });
+//   } else {
+//     imagesToLoad.forEach((img) => {
+//       loadImages(img);
+//     });
+//   }
