538c109b6f0fe5287937149560da93d6a4cefd0f
Nwokolo24 <nwokolobueze@gmail.com>
2020-03-01 02:46:27 -0700

    fixed the weather condition tiles

--- weather/js/function.js
+++ weather/js/function.js
@@ -105,7 +105,7 @@ let temp = 60;
 let speed = 4.8;
 buildWC(speed, temp);
 //The Time Indicator function
-let hour="10";
+let hour="6";
 timeBall(hour);
 console.log(hour);
 //Background image change
