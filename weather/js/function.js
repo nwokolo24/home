431dfb6b3d08b7123b11627580c974f72272ab3d
Nwokolo24 <nwokolobueze@gmail.com>
2020-03-05 18:21:11 -0700

    getHour function built

--- weather/js/function.js
+++ weather/js/function.js
@@ -20,7 +20,8 @@ document.addEventListener("DOMContentLoaded", function(){
   buildModDate();
   const menuButton = document.querySelector("#menu-button");
   menuButton.addEventListener('click', burgerMenu);
-
+  //call current date
+  buildCurDate(); 
 //Variables for wind chill function
 let temp = 31;
 let speed = 4.8;
@@ -54,9 +55,10 @@ fetchWeatherData(weatherURL);
 * WEATHER SITE JAVASCRIPT FUNCTIONS *
 ************************************* */
  //Js to get the Current Date
+ function buildCurDate(){
 const options = {weekday: "long", day: "numeric", month:"short", year: "numeric"};
 document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
-
+ }
 /* ##################################################
                Fetch Weather Data
 ###################################################### */
@@ -81,6 +83,11 @@ function fetchWeatherData(weatherURL){
     //************ Get the location information *********
     let locName = p.properties.relativeLocation.properties.city;
     let locState = p.properties.relativeLocation.properties.state;
+    let lowTemp = p.properties.relativeLocation.properties.lowTemp;
+    let temperature = p.properties.relativeLocation.properties.temperature;
+    let windSpeed = p.properties.relativeLocation.properties.windSpeed;
+    let windGust = p.properties.relativeLocation.properties.windGust;
+    let highTemp = p.properties.relativeLocation.properties.highTemp;
 
     //put them together
     let fullName = locName+', '+locState;
@@ -97,12 +104,26 @@ function fetchWeatherData(weatherURL){
     // and store it into local storage.
     const prestonData = JSON.stringify({fullName,latLong});
     console.log(prestonData);
-    sessStore.setItem("PrestonID", prestonData);
+    locStore.setItem("PrestonID", prestonData);
 
 
     //************ Get the current condition information *********
     //As the data is extracted from the JSON, store it into session storage
     //Get the temperature data
+    sessStore.setItem("FullName", fullName);
+    sessStore.setItem("latLong", latLong);
+    sessStore.setItem("lowTemp", lowTemp);
+    sessStore.setItem("temperature", temperature);
+    
+
+    // Get the wind data 
+    sessStore.setItem("windSpeed", windSpeed);
+    sessStore.setItem("windGust", windGust);
+    sessStore.setItem("highTemp", highTemp);
+
+    // Get the hourly data using another function - should include the forecast temp, condition icons and wind speeds. 
+    //The data will be stored into session storage.
+    getHourly(p.properties.forecastHourly);
   })
   .catch(function(error){
     console.log("There was a fetch problem: ", error.message);
@@ -110,6 +131,49 @@ function fetchWeatherData(weatherURL){
 
   })
 
+}
+
+/* ************************************
+*  Get Hourly Forecast data
+************************************* */
+function getHourly(URL){
+  fetch(URL)
+  .then(function(response){
+    if(response.ok){
+      return response.json();
+    }
+    throw new Error("Response not OK.");
+  })
+  .then(function(data){
+    console.log("Data from getHourly function:");
+    console.log(data); //Let's see what we got back
+
+    //Store 12 hours of data to session storage
+    var hourData = [];
+    let todayDate = new Date();
+    var nowHour = todayDate.getHours();
+    console.log(`nowHour is ${nowHour}`);
+    for (let i = 0, x = 11; i <= x; i++){
+      if (nowHour < 24) {
+        hourData[nowHour] = data.properties.periods[i].temperature + "," + data.properties.periods[i].windSpeed + "," + data.properties.periods[i].icon;
+        sessStore.setItem(`hour${nowHour}`, hourData[nowHour]);
+        nowHour++;
+      } else {
+        nowHour = nowHour - 12;
+        hourData[nowHour] = data.properties.periods[i].temperature + "," + data.properties.periods[i].windSpeed + "," + data.properties.periods[i].icon;
+        sessStore.setItem(`hour${nowHour}`, hourData[nowHour]);
+        nowHour = 1;
+      }
+    }
+
+    // Get the shortForecast value from the first hour (the current hour)
+    // This will be the condition keyword for setting the background image
+    sessStore.setItem(`shortForecast`, data.properties.periods[0].shortForecast);
+    
+    //Call the buildPage function
+    buildPage();
+  })
+  .catch(error => console.log("There was a getHourly error: ", error))
 }
 
 //Js to get the last modified date
@@ -192,6 +256,7 @@ function changeSummaryImage(curCond){
 let selectImage = document.querySelector(".clear");
 selectImage.classList.add(curCond);
 }
+
 
 
 // // // Integers evenly divisible by divisor
