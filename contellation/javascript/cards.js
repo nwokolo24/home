5499ba652ce1aab711e17590765b13e360279ac2
Nwokolo <nwokolobueze@gmail.com>
2021-01-17 16:37:32 -0700

    CleanUp

--- contellation/javascript/cards.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $('.const-card').hover(function(){
-      $('#show-here').addClass('show-info');
-  });
-})
--- python/hello.py
+++
@@ -1,2 +0,0 @@
-value = input("Enter something here: ")
-print("The data type of", value, "is", type(value))
